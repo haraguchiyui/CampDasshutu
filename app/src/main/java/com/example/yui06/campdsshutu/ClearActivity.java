a3c7e88099cffab008b89fa5cfe665d4d9b28b5a
haraguchiyui <yuimaruuu.06.10@gmail.com>
2018-06-02 20:36:44 +0900

    dasshutu

---
+++ app/src/main/java/com/example/yui06/campdsshutu/ClearActivity.java
@@ -0,0 +1,13 @@
+package com.example.yui06.campdsshutu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clear);
+    }
+}
